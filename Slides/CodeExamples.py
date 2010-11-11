1bc43b3b6cb42e67657f8e6a525f12e1d2639169
Rob Tirrell <r.tirrell@gmail.com>
2010-11-11 15:43:01 -0800

    Start on slides.

---
+++ Slides/CodeExamples.py
@@ -0,0 +1,19 @@
+print 'Hello World'
+
+some_list = [12, 'monkeys']
+
+some_dict = {
+  'New York': 'A state in the US.', 
+  42: 'A completely irrelevant number.', 
+  today_is_sunday: false
+} 
+
+some_string = 'julius'
+some_string = some_string.upper()
+# some_string is now 'JULIUS'
+
+some_dict[42]
+# >>> 'A completely irrelevant number'
+
+some_string[1]
+# >>> 'U'
